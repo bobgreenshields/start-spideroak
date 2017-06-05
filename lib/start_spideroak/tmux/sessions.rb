79d44ae7a9392f29a8d9122cfd4fda1f6aa9ee84
Bob Greenshields <bob@thegreenshields.com>
2017-06-05 08:09:12 +0100

    changed name to session_name

--- lib/start_spideroak/tmux/sessions.rb
+++ lib/start_spideroak/tmux/sessions.rb
@@ -35,23 +35,22 @@ module StartSpideroak
 				@sessions.any? { | session | session.name?(session_name) }
 			end
 
-			def create(name)
-				return if include?(name)
-				@cmd_runner.call("tmux new-session -d -s #{name}")
+			def create(session_name)
+				return if include?(session_name)
+				@cmd_runner.call("tmux new-session -d -s #{session_name}")
 				@cmd_runner.on_failure { raise TmuxSessionCreateFailedError,
-					"Tmux::Sessions#create failed to create a session called #{name}" }
-				@cmd_runner.on_success { check_new_session_exists(name) }
+					"Tmux::Sessions#create failed to create a session called #{session_name}" }
+				@cmd_runner.on_success { check_new_session_exists(session_name) }
 			end
 
-			def check_new_session_exists(name)
+			def check_new_session_exists(session_name)
 				load_sessions
-				unless include?(name)
+				unless include?(session_name)
 					raise TmuxSessionCreateFailedError,
 						"Tmux::Session::Create seemed to succeed" \
-						"but a session called #{name} could not be found"
+						"but a session called #{session_name} could not be found"
 				end
 			end
-			
 		end
 	end
 end
