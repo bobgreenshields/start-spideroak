94956497bf56e04a2d36a8876e3f1f99282fe873
Bob Greenshields <bob@thegreenshields.com>
2017-06-21 08:56:35 +0100

    finish testing #include_session?

--- spec/start_spideroak/tmux/app_spec.rb
+++ spec/start_spideroak/tmux/app_spec.rb
@@ -10,6 +10,25 @@ describe Tmux::App do
 				expect(app.include_session?("unknown_session_name")).to be_falsey
 			end
 		end
+
+		context "when named session is present" do
+			let (:cmd_mock) { CmdRunnerMock.success("guake: 1 windows\nnext: 2 windows", occurs: 2) }
+			let (:app) { Tmux::App.new(cmd_mock) }
+
+			it "returns true" do
+					expect(app.include_session?("guake")).to be_truthy
+			end
+		end
+
+		context "when named session is not present" do
+			let (:cmd_mock) { CmdRunnerMock.success("guake: 1 windows\nnext: 2 windows", occurs: 2) }
+			let (:app) { Tmux::App.new(cmd_mock) }
+
+			it "returns false" do
+				expect(app.include_session?("unknown_session_name")).to be_falsey
+			end
+		end
+
 	end
 	
 end
